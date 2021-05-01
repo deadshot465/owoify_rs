edbc0f8126718b6dd1f79514d354457d6ad160b3
Chehui Chou <deadshot465@gmail.com>
2021-05-01 17:24:12 +0900

    Add tests.

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,54 @@ mod traits;
 mod utility;
 pub use traits::Owoifiable;
 pub use traits::OwoifyLevel;
+
+#[cfg(test)]
+mod tests {
+    use crate::{Owoifiable, OwoifyLevel};
+
+    const SOURCE: &str = "Hello World! This is the string to owo! Kinda cute, isn't it?";
+
+    #[test]
+    fn test_owoify() {
+        assert_ne!(SOURCE.owoify(&OwoifyLevel::Owo), SOURCE.to_string());
+    }
+
+    #[test]
+    fn test_owo() {
+        assert_ne!(SOURCE.owoify(&OwoifyLevel::Owo), "".to_string());
+    }
+
+    #[test]
+    fn test_uwu() {
+        assert_ne!(SOURCE.owoify(&OwoifyLevel::Uwu), "".to_string());
+    }
+
+    #[test]
+    fn test_uvu() {
+        assert_ne!(SOURCE.owoify(&OwoifyLevel::Uvu), "".to_string());
+    }
+
+    #[test]
+    fn test_owo_not_equal_to_uwu() {
+        assert_ne!(
+            SOURCE.owoify(&OwoifyLevel::Owo),
+            SOURCE.owoify(&OwoifyLevel::Uwu)
+        );
+    }
+
+    #[test]
+    fn test_owo_not_equal_to_uvu() {
+        assert_ne!(
+            SOURCE.owoify(&OwoifyLevel::Owo),
+            SOURCE.owoify(&OwoifyLevel::Uvu)
+        );
+    }
+
+    #[test]
+    fn test_uwu_not_equal_to_uvu() {
+        assert_ne!(
+            SOURCE.owoify(&OwoifyLevel::Uwu),
+            SOURCE.owoify(&OwoifyLevel::Uvu)
+        );
+    }
+}
